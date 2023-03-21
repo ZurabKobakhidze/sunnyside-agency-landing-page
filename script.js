d9f344e7a7b16497911296b038136ed4ebfb372f
Zukak <zukakobakhidze@gmail.com>
2023-03-21 10:44:56 +0400

    change container 3 styles

--- script.js
+++ script.js
@@ -1,9 +1,12 @@
 const hamburger = document.querySelector(".hamburger");
 const menu = document.querySelector(".menu_mobile");
 
+
 hamburger.addEventListener("click", () => {
   
   hamburger.classList.toggle("hamburger_active");
   
     menu.classList.toggle("menu_mobile_active");
+    
 });
+
